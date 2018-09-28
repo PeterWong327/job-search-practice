e1dcef16041ce8b97238754ad249f57575de4575
“Peter <peterwongisnow@gmail.com>
2018-09-28 16:48:26 -0700

    Linked list add two numbers leetcode

--- leetCode/09282018.js
+++ leetCode/09282018.js
@@ -20,3 +20,56 @@ var twoSum = function(nums, target) {
       }
     }
 };
+
+
+
+
+// You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes
+// contain a single digit. Add the two numbers and return it as a linked list.
+//
+// You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+//
+// Example:
+//
+// Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+// Output: 7 -> 0 -> 8
+// Explanation: 342 + 465 = 807.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+var addTwoNumbers = function(l1, l2) {
+    let newl1 = "";
+    let newl2 = "";
+    let current1 = l1;
+    let current2 = l2;
+
+    while (current1 !== null) {
+      newl1 += current1;
+      current1 = current1.next;
+    }
+
+    while (current2 !== null) {
+      newl2 += current2;
+      current2 = current2.next;
+    }
+
+    let result = parseInt(newl1) + parseInt(newl2);
+    let newResult = result.toString();
+    newResult = newResult.split("").reverse().join("");
+
+    // newResult = 708
+    // need to initialize a new node
+    let head = newResult[0];
+
+    for (let i = 1; i < newResult.length; i += 1) {
+      head.next = newResult[i];
+    }
+
+    return head;
+};
