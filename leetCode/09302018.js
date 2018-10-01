5ecd629243df3dcb3b5cae346771ed8ab537acad
“Peter <peterwongisnow@gmail.com>
2018-09-30 23:18:31 -0700

    Complete leetcode challenge: reverse integer

--- leetCode/09302018.js
+++ leetCode/09302018.js
@@ -4,16 +4,32 @@
 //
 // Input: 123
 // Output: 321
+
 // Example 2:
 //
 // Input: -123
 // Output: -321
+
 // Example 3:
 //
 // Input: 120
 // Output: 21
+
 // Note:
 // Assume we are dealing with an environment which could only store
 // integers within the 32-bit signed integer range: [−231,  231 − 1].
 // For the purpose of this problem, assume that your function returns 0
 // when the reversed integer overflows.
+
+var reverse = function(x) {
+    let string = x.toString();
+    let newString = "";
+
+    for (let i = string.length - 1; i >= 0; i -= 1) {
+      if (string[i] !== "0") {
+        newString += string[i];
+      }
+    }
+
+    return parseInt(newString);
+};
