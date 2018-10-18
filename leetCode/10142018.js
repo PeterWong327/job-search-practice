3bd6729fef8eaa907b2f3adbb3a2975713044ab5
“Peter <peterwongisnow@gmail.com>
2018-10-18 12:42:45 -0700

    Continue working on lengthOfLongestSubstring

--- leetCode/10142018.js
+++ leetCode/10142018.js
@@ -21,19 +21,36 @@
 //              Note that the answer must be a substring, "pwke" is a
 //              subsequence and not a substring.
 
+// "dvdf" => 3 ("vdf")
+// "bwf" => 3
 
 var lengthOfLongestSubstring = function(s) {
+  if (s.length <= 1) {
+    return s.length;
+  }
+
   let longest = "";
   let currStr = "";
 
-  for (let i = 0; i < s.length; i += 1) {
-    let currentChar = s[i];
-    if (!currStr.includes(currentChar) && currStr.length === 0) {
-      currStr += currentChar;
-    } else if (!longest.includes(curr)) {
-      longest = "";
-      longest += currentChar;
+  for (let i = 0; i < s.length - 1; i += 1) {
+    currStr = "" + s[i];
+
+    for (let j = i + 1; j < s.length; j += 1) {
+      console.log("currStr is:" + currStr + " when i is:" + i + " and j is:" + j);
+      let currentChar = s[j];
+      if (!currStr.includes(currentChar)) {
+        currStr += currentChar;
+      } else if (currStr.includes(currentChar)) {
+        if (currStr.length > longest.length) {
+          longest = currStr;
+          j = s.length;
+          // currStr = "" + currentChar;
+        }
+      }
     }
+  }
+  if (currStr.length > longest.length) {
+    longest = currStr;
   }
 
   return longest;
