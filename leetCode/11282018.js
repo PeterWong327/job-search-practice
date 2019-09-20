97c4021a31587e446bdccc68ab5e1ee2d7bc68cf
“Peter <peterwongisnow@gmail.com>
2019-09-19 23:51:51 -0700

    Start Reverse Words in a String III

--- leetCode/11282018.js
+++ leetCode/11282018.js
@@ -12,10 +12,34 @@
 // If you have figured out the O(n) solution, try coding another solution
 // using the divide and conquer approach, which is more subtle.
 
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var maxSubArray = function(nums) {
+  // iterate through each number
+  // save the first element as the largest variable
+  // save the largest variable's index as the 'startIdx'
+  // check if the next element added to the largest var is greater
+    // if greater, then save sum as new largest, then
+    // if less, save 'endIdx' as current index.
 
+    let largest = nums[0];
+    let startIdx = 0;
+    let endIdx = 0;
+
+    for (let i = 0; i < nums.length - 1; i++) {
+      let next = nums[i + 1];
+      let curr = nums[i];
+
+      if (curr > largest) {
+        largest = curr;
+        startIdx = i;
+      }
+
+      if ((largest + next) >= largest) {
+        largest += next;
+        endIdx = i + 1;
+      } else if ((largest + next) < largest) {
+        endIdx = i;
+      }
+    }
+
+    return nums.slice(startIdx, endIdx + 1);
 };
